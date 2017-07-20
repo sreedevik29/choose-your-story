8ae1db4467c79f17c968d8d5d6ab453aa599b511
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-07-20 16:20:46 -0400

    Add all story options in txt files and adapted code to fit all storylines

--- story.py
+++ story.py
@@ -1,3 +1,7 @@
+import subprocess
+
+from stories import *
+
 def story():
 	active = True
 	while active:
@@ -5,31 +9,49 @@ def story():
 		intro_input = raw_input("If you choose the forest, type in 'left' or 'l' or to continue alongside the sea, type 'right' or 'r': ")
 		if intro_input == "left" or intro_input == "l":
 			print("Forest option text")
-			forest_input = raw_input("Type 'reveal' or 'r' to reveal the shadow or 'walk' or 'w' to continue walking")
+			forest_input = raw_input("Type 'reveal' or 'r' to reveal the shadow or 'walk' or 'w' to continue walking: ")
 			if forest_input == "r" or forest_input == "reveal":
-				print("Wizard option text")
-				riddle_input = raw_input("What's the ridde?")
-				if riddle_input == "honeybun":
-					print("Fulfillment advice text")
-					active = False
-				else:
-					print("Wizard attack and die")
-					active = False
+				print("reveal-shadow")
+				wizard_input == raw_input("Type 'r'/'run' to run from the wizard or 'f'/'face' to face it: ")
+				if wizard_input == "face" or "f":
+					print("Face wizard text")
+					riddle_input = raw_input("Type your answer here ")
+					if riddle_input == "steps" or ridde_input == "footsteps":
+						print("correct-answer-conclusion")
+						active = False
+					elif riddle_input = "h" or riddle_input == "hint"
+						print("hint-riddle ")
+						riddle_input = raw_input("Type your answer here ")
+						if riddle_input == "steps" or ridde_input == "footsteps":
+							print("correct-answer-conclusion")
+							active = False
+					else:
+						print("Wizard attack")
+						active = False
 			else: 
 				print("Shadow attack and die")
 				active = False
 		elif intro_input == "right" or intro_input == "r":
 			print("Sea option text")
-			sea_input = raw_input("Type 'w' or 'water' to go into the water or 'l' or 'land' to get out")
+			sea_input = raw_input("Type 'w' or 'water' to go into the water or 'l' or 'land' to get out: ")
 			if sea_input == "l" or sea_input == "land":
-				print("Back to land text")
-				riddle_input = raw_input("What's the riddle?")
-				if riddle_input == "honeybun":
-					print("Fulfillment advice text")
-					active = False
-				else:
-					print("Wizard attack and die")
-					active = False
+				print("run to land")
+				wizard_input == raw_input("Type 'r'/'run' to run from the wizard or 'f'/'face' to face it: ")
+				if wizard_input == "face" or "f":
+					print("Face wizard text")
+					riddle_input = raw_input("Type your answer here ")
+					if riddle_input == "steps" or ridde_input == "footsteps":
+						print("correct-answer-conclusion")
+						active = False
+					elif riddle_input = "h" or riddle_input == "hint"
+						print("hint-riddle ")
+						riddle_input = raw_input("Type your answer here ")
+						if riddle_input == "steps" or ridde_input == "footsteps":
+							print("correct-answer-conclusion")
+							active = False
+					else:
+						print("Wizard attack")
+						active = False
 			else:
 				print("Water demon attack")
 				active = False
@@ -38,7 +60,13 @@ def story():
 
 
 def main():
+	f = open("stories/intro.txt", "r")
+	introduction = f.read()
+	f.close()
 	story()
+
+	with open('stories/intro.txt', 'r') as f:
+     	story_content['intro'] = f.read()
 
 
 if __name__ == '__main__':
